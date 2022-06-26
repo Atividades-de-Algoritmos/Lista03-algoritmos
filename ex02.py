2fdde1dff9a5e92c8fdab368fe2a5b8d3cf75870
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-26 14:30:54 -0300

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -1,3 +1,11 @@
+
+# Autores:
+# Michel Silva
+# Emanuel Frank
+# Carlos Eduardo
+# data: 26/06/2022
+#
+
 # Faça um Programa que peça os 3 lados de um triângulo.
 # O programa deverá informar se os valores podem ser um triângulo.
 # Indique, caso os lados formem um triângulo, se ele é: equilátero, isósceles ou escaleno.
