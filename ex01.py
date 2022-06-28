84c251a10ad5313ff2b1260c66df66f5dbd36b1e
Carlos Eduardo ✔ <97265556+SmokeDevL@users.noreply.github.com>
2022-06-28 01:28:36 -0300

    Calculando Delta Δ

--- ex01.py
+++ ex01.py
@@ -3,11 +3,11 @@
 # Michel Silva
 # Emanuel Franklyn
 # Carlos Eduardo
-# data: 26/06/2022
+# data: 27/06/2022
 #
 
-print("informe os valores de a, b e c") # Introduzindo o código
-print("exemplo: aX^2 + bX + c = 0\n")
+print("Informe os valores de a, b e c") # Introduzindo o código
+print("Exemplo: aX^2 + bX + c = 0\n") # Dando Exemplos
 
 # Entrada de dados
 
@@ -17,28 +17,29 @@ c = int(input("informe o valor de c: ")) # Solicitando um valor inteiro para c
 
 # Processamento de dados
 
-delta = b**2 - 4*a*c  #calculo do delta
+delta = b**2 - 4*a*c  # Calculo do delta
 
-# se delta for negativo, não existe raiz
-# se delta for igual a zero, existe apenas uma raiz ou duas raízes iguais
-# se delta for positivo, existe duas raízes
+# Lógica de programação
+# 1 - Se delta for negativo, não existe raiz
+# 2 - Se delta for igual a zero, existe apenas uma raiz ou duas raízes iguais
+# 3 - Se delta for positivo, existe duas raízes
 
 # Saída de dados
 
-if delta < 0: #se delta for negativo
+if delta < 0: # Se delta for negativo
   print(f"\ndelta = {delta}")
   print("não existe raiz!")
 
-elif delta == 0: #se delta for igual a zero
+elif delta == 0: # Se delta for igual a zero
   print(f"\ndelta = {delta}")
-  x1 = x2 = (-b + (delta**(1/2))/(2*a)) #calculo da raiz, x1 = x2
+  x1 = x2 = (-b + (delta**(1/2))/(2*a)) # Calculo da raiz, x1 = x2
   print(f"a raiz x1 é {x1}")
   print(f"a raiz x2 é {x2}")
 
-else: #se delta for positivo
+else: # Se delta for positivo
   print(f"\ndelta = {delta}")
-  x1 = (-b + (delta**(1/2))/(2*a)) #calculo da raiz x1
-  x2 = (-b - (delta**(1/2))/(2*a)) #calculo da raiz x2
+  x1 = (-b + (delta**(1/2))/(2*a)) # Calculo da raiz x1
+  x2 = (-b - (delta**(1/2))/(2*a)) # Calculo da raiz x2
   print(f"a raiz x1 é {x1}")
   print(f"a raiz x2 é {x2}")
 
