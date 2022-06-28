64b22a578328451c28feee0e3c7e15f8b18251b8
Carlos Eduardo ✔ <97265556+SmokeDevL@users.noreply.github.com>
2022-06-28 01:35:51 -0300

    Lados de um triângulo 🔺

--- ex02.py
+++ ex02.py
@@ -3,10 +3,11 @@
 # Michel Silva
 # Emanuel Frank
 # Carlos Eduardo
-# data: 26/06/2022
+#
+# data: 27/06/2022
 #
 
-# Faça um Programa que peça os 3 lados de um triângulo.
+# 2 - Faça um Programa que peça os 3 lados de um triângulo.
 # O programa deverá informar se os valores podem ser um triângulo.
 # Indique, caso os lados formem um triângulo, se ele é: equilátero, isósceles ou escaleno.
 
@@ -24,21 +25,21 @@ ladoC = int(input("informe o lado C: ")) # Solicitando um valor inteiro para o L
 # Se for, esses três valores não formam um triângulo e acabou o programa aí.
 
 if (ladoA + ladoB < ladoC) or (ladoA + ladoB < ladoC) or (ladoA + ladoB < ladoC):
-  print("não é um triângulo!")
+  print("\nNão é um triângulo!")
 
 # Se não for, precisamos testar se é equilátero, isósceles ou escaleno.
 
 elif (ladoA == ladoB) and (ladoA == ladoC):
-  print("é um triângulo equilátero!")
+  print("É um triângulo equilátero!")
 
-# Note que não precisamos comparar os lados b e c, pois se a é igual a b E TAMBÉM (operador lógico and) a é igual a c, então o lado b vai ser automaticamente igual ao lado c.
+# Note que não precisamos comparar os lados A e C, pois se ladoA é igual a ladoB e ladoA é igual a ladoC, então o ladoB vai ser automaticamente igual ao ladoC.
 # Se forem todos iguais, diz que é equilátero e acabou aí.
 # Se não for equilátero, cai no próximo elif.
-# Lá vamos testar se ele tem dois lados iguais: a==b ou a==c ou b==c (notem o OU, que é o operador lógico or).
+# Lá vamos testar se ele tem dois lados iguais: a == b ou a == c ou b == c (notem o OU, que é o operador lógico or).
 # Se alguma dessas comparações retornar verdadeiro, o triângulo é isósceles e acaba aí.
 
 elif (ladoA == ladoB) or (ladoA == ladoC) or (ladoB == ladoC):
-  print("é um triângulo isósceles!")
+  print("É um triângulo isósceles!")
 
 # Porém, se não for isósceles, cai no else final.
 # Pois se não é equilátero nem isósceles, e é um triângulo, tem de ser escaleno.
